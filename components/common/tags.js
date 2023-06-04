327d76476e7d143ace9e040d03d0446fb6d72ae9
mahdaoui <a.mahdaoui@sanadtech.com>
2023-06-04 16:29:09 +0100

    handle articles empty tags

--- components/common/tags.js
+++ components/common/tags.js
@@ -5,9 +5,12 @@ import styles from './styles/common.module.css';
 const Tag = ({ tagName }) => <span className={styles.tag}>{tagName}</span>;
 
 export default function Tags({ tags }) {
-  const tagsArray = tags.split(',').filter((n) => n);
-  const tagsList = tagsArray.map((tag, index) => {
-    return <Tag key={nanoid()} tagName={tag}></Tag>;
-  });
-  return <div>{tagsList}</div>;
+  if (!tags) return <></>;
+  else {
+    const tagsArray = tags.split(',').filter((n) => n);
+    const tagsList = tagsArray.map((tag, index) => {
+      return <Tag key={nanoid()} tagName={tag}></Tag>;
+    });
+    return <div>{tagsList}</div>;
+  }
 }
